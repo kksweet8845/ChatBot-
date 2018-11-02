c00fbc270f65351c5b2070f027930558db00207a
kksweet8845 <s0913768710@gmail.com>
2018-11-02 11:45:34 +0800

    Upload file in back_end folder

--- back_end/server.js
+++ back_end/server.js
@@ -7,6 +7,7 @@ const path = '../front_end';
 
 // about us website
 app.use(express.static(path + '/aboutUs'));
+app.use(express.static(path + '/accessFile'));
 
 app.listen(port,() => {
   console.log('Listening on port:',port);
