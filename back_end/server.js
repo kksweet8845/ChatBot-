bfd48d293c61b53a4fbfc05cf94b6e0635cb469e
kksweet8845 <s0913768710@gmail.com>
2018-11-14 15:34:42 +0800

    Upload server with background running

--- back_end/server.js
+++ back_end/server.js
@@ -8,7 +8,7 @@ const path = '../front_end';
 // about us website
 app.use(express.static(path + '/aboutUs'));
 app.use(express.static(path + '/accessFile'));
-
+app.use(express.static(path + '/WebChatBotLayout'));
 app.listen(port,() => {
   console.log('Listening on port:',port);
 });
