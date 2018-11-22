2b86f556ed83f2494b664d7d73c66fb59962552d
kksweet8845 <s0913768710@gmail.com>
2018-11-23 00:17:08 +0800

    upgrade the uploading file

--- back_end/uploadFile/upload_file.js
+++ back_end/uploadFile/upload_file.js
@@ -4,13 +4,16 @@ const formidable = require('formidable');
 const http = require('http');
 const fs = require('fs');
 
+
 const port = 12224;
 
 
 var srv = http.createServer((req,res)=>{
   if(req.url == '/fileUpload' ){
     var form = new formidable.IncomingForm();
+    form.uploadDir = __dirname;
     form.parse(req,(err,fields,files)=>{
+      console.log(fields);
       var oldPath = files.filetoupload.path;
       var newPath = __dirname + '/data/test';
       fs.rename(oldPath,newPath,(err)=>{
@@ -23,6 +26,7 @@ var srv = http.createServer((req,res)=>{
   }else {
     res.writeHead(200,{'Content-Type':'text/html'});
     res.write('<form action="fileUpload" method="post" enctype="multipart/form-data">');
+    res.write('File name: <input type="text" name="fileName"><br>')
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
