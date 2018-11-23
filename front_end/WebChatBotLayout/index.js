23500e883df4da9c9eb3391623d1d61fd468378a
happy210168 <34540770+happy210168@users.noreply.github.com>
2018-11-24 06:49:07 +0800

    Bubble & fix font bug
    Bubble & fix fontsize bug

--- front_end/WebChatBotLayout/index.js
+++ front_end/WebChatBotLayout/index.js
@@ -1,35 +1,23 @@
 $(document).ready(function() {  
     $("#ajax_form button").click(function(event) {
         event.preventDefault(); 
-         var fname = $('#fname').val(); //get fname(input)
         $.ajax({
             method:"GET",
             url: "https://randomuser.me/api/"
         })
         .done(function(msg){
-          var item =""; //User's Bubble(input)
-          var item2 =""; //Chatbot's Bubble(reply)
-          item2 = '<div class="arrow_box1"><h1 class="bubble">' + 'hello' + '</div></h1>';         
-          var row = Math.floor(fname.length/12); //row number in one bubble
-          if(fname.length!=0){
-            for(var i=0;i<row+1;i++){
-              var temp = fname.substr(i*12,12); //split string
-              if(i==0){ 
-                item += '<div class="arrow_box"><h1 class="bubble">' + temp;
-                if(row!=0) item += '<br/>';
-              }
-              else if(i!=row) item += temp + '<br/>';
-              else item += temp ;
-            }
-            item += '</div></h1>';
-            console.log(item);
-            console.log(item2);
-            $('.Window').append(item); //append user's bubble
-            $('.Window').append(item2); //append chatbot's bubble
+          var input = $('#fname').val(); //get fname(input)
+          var item = '<div class="arrow_box"><h1 class="bubble">' + input + '</div></h1>'; //Chatbot's Bubble(reply); //User's Bubble(input)     
+          console.log(item);
+          $('.Window').append(item); //append user's bubble
 
-            var div = document.getElementById('window');  
-            div.scrollTop = div.scrollHeight; //auto scroll to bottom
-          }
+          var reply = 'Hello Hello Hello'   //String for system 
+          var item2 = '<div class="arrow_box1"><h1 class="bubble">' + reply + '</div></h1>'; //Chatbot's Bubble(reply)  
+          console.log(item2);
+          $('.Window').append(item2); //append chatbot's bubble
+
+          var div = document.getElementById('window');  
+          div.scrollTop = div.scrollHeight; //auto scroll to bottom
       });
     });
 }); 
