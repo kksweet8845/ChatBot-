64d8aa19586744f77be1bf56d9bf442f418b906c
kksweet8845 <s0913768710@gmail.com>
2018-11-30 11:03:10 +0800

    modified sever.js index.html bubble.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+var bigram = require('./algorithm/bigram');
+var formidable = require('formidable');
+var fs = require('fs');
 const port = 11022;
 
 var app = express();
@@ -9,6 +12,21 @@ const path = '../front_end';
 app.use(express.static(path + '/aboutUs'));
 app.use(express.static(path + '/accessFile'));
 app.use(express.static(path + '/WebChatBotLayout'));
+
+
+app.post('/testQuest',(req,res)=>{
+      var form = new formidable.IncomingForm();
+      form.parse(req,(err,fields,files)=>{
+        var question = fields['userQ'];
+        var obj = JSON.parse(fs.readFileSync(__dirname + '/uploaded/jsonFile/defaultQA.json','utf8'));
+        bigram.evalQuery(question,obj,(err,max_index)=>{
+          if(err) return res.send(err);
+          res.send(obj.conversations[max_index].A);
+        });
+      });
+});
+
+
 app.listen(port,() => {
   console.log('Listening on port:',port);
 });
--- front_end/WebChatBotLayout/js/bubble.js
+++ front_end/WebChatBotLayout/js/bubble.js
@@ -2,22 +2,27 @@ $(document).ready(function() {
     $("#ajax_form button").click(function(event) {
         event.preventDefault(); 
         $.ajax({
-            method:"GET",
-            url: "https://randomuser.me/api/"
+            method:"POST",
+            url: '/testQuest',
+            data:{
+              userQ: $('#fname').val() 
+            },
+            success: reply
         })
-        .done(function(msg){
-          var input = $('#fname').val(); //get fname(input)
-          var item = '<div class="arrow_box"><h1 class="bubble">' + input + '</div></h1>'; //Chatbot's Bubble(reply); //User's Bubble(input)     
-          console.log(item);
-          $('.Window').append(item); //append user's bubble
-
-          var reply = 'Hello Hello Hello'   //String for system 
-          var item2 = '<div class="arrow_box1"><h1 class="bubble">' + reply + '</div></h1>'; //Chatbot's Bubble(reply)  
-          console.log(item2);
-          $('.Window').append(item2); //append chatbot's bubble
-
-          var div = document.getElementById('window');  
-          div.scrollTop = div.scrollHeight; //auto scroll to bottom
-      });
     });
+
+    var reply = (data)=>{
+        var input = $('#fname').val();
+        var item = '<div class="arrow_box"><h1 class="bubble">'+ input + '</h1></div>';
+        
+        $('.Window').append(item);
+        
+        var reply = data;
+        var item2 = '<div class="arrow_box1"><h1 class="bubble">' + reply + '</h1></div>';
+
+        $('.Window').append(item2);
+
+          var div = document.getElementById('window');
+          div.scrollTop = div.scrollHeight;
+    }
 }); 
